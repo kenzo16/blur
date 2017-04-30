3e02cb79e4c02c5d922ade379e27416d5024b95e
Asmeer Mohammed M <asmeer.me@live.in>
2017-04-30 19:56:47 +0530

    added blinddeconv folder path

--- createData.m
+++ createData.m
@@ -2,24 +2,27 @@
 %CREATE DATASET OF BLUR KERNALS FOR BOTH BLUR TYPES
 M = 0;
 dirname = 'dataset'
-
+addpath(genpath('blindDeconvolution'));
 M=0;
-for i = 1 : 7
+
+for i = 1 : 1
     sprintf('Running on O%d',i);
     pic = sprintf('%s/O%d.jpg',dirname,i);
     img = (rgb2gray(imread(pic)));
     getDataSet(img , 'O' , M+1,'dataset_O.mat');
     load('dataset_O.mat');
+    save('i.mat','i');
     [M N]=size(dataset);
 end
 
-
-for i = 1 : 9
+M=0;
+for i = 1 : 1
     sprintf('Running on M%d',i);
     pic = sprintf('%s/M%d.jpg',dirname,i);
     img = (rgb2gray(imread(pic)));
     getDataSet(img , 'M' , M+1,'dataset_M.mat');
     load('dataset_M.mat');
+    save('i.mat','i');
     [M N]=size(dataset);
 end
 
