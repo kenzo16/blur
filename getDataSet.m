8decdae2eead620de9b7f53b6402471592778c2b
Asmeer Mohammed M <asmeer.me@live.in>
2017-05-01 22:15:35 +0530

    -imhist removed

--- getDataSet.m
+++ getDataSet.m
@@ -3,7 +3,7 @@ function [] = getDataSet( img , val , count , file )
 %SAME BLIND DECONVOLUTION AND ASSIGNS 'M' or 'O' for Motion blur or Out-of-focus blur respectively
     sig_noise=0.01;
     load test_data/sizeL
-
+    load(file);
     [M N]=size(img);
 
     n=64; % block size
@@ -12,7 +12,7 @@ function [] = getDataSet( img , val , count , file )
     i=0;j=0;
     p=floor(M/m);
     q=floor(N/n);
-    dataset = zeros(1,3);
+    
 
     while i <= p
         while j <= q
@@ -40,27 +40,30 @@ function [] = getDataSet( img , val , count , file )
             [k]=deconv_diagfe_filt_sps(pic,psfsize,psfsize,sig_noise,[],0);
             %[k]=deconv_freq_filt_gauss(pic,psfsize,psfsize,sig_noise,[],0);
 
-            k = cropmatrix(k, 0.05);
+            k = cropmatrix(k, 0.00);
 
             demoimg = imresize(k,[100 100]);
+
             %imshow(demoimg);
 
             %NORMALIZING
             normA = demoimg - min(demoimg(:));
-            normA = normA ./ max(normA(:));
-            A = imhist(normA);
-            A = A/sum(A);
+            normA = normA / sum(normA(:));
+            
+            %histogram is unnecessary
+            %A = imhist(normA);
+            %A = A/sum(A);
 
             %feature extraction
-            [ gamma, sigma ] = feature_extract( A );
-
+            [ gamma, sigma ] = feature_extract2( normA );
+            
             dataset(count,1) = gamma;
             dataset(count,2) = sigma;
             dataset(count,3) = val;
             save(file,'dataset');
             count=count+1;
             j=j+1;
-           
+
         end
 
         i=i+1;
